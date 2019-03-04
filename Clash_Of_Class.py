f6f9c9481baeeaf1091e16a9e5ed19ec189ff557
jeankillian <jeankillian@yahoo.fr>
2019-03-04 15:32:48 +0100

    MAJ2-fonctionnel

--- Clash_Of_Class.py
+++ Clash_Of_Class.py
@@ -2,137 +2,148 @@ import random
 
 
 class Character:
-    Max_hp = 12
+    max_hp = 12
+
+    def __init__(self, name):
+        self.name = name
+        self.current_life_point = self.max_hp
 
 
 class Warrior(Character):
-    name = "Gimli"
-    Sword = 12
-    Magic = 8
-    Bow = 10
+    chara_class = "warrior"
+    sword = 12
+    magic = 8
+    bow = 10
 
-    def atk(test):
-        sword_dice = random.randint(1, Warrior.Sword)
-        magic_dice = random.randint(1, Warrior.Magic)
-        bow_dice = random.randint(1, Warrior.Bow)
-        print(str(Warrior.name) + " attack!")
+    def attack(self):
+        sword_dice = random.randint(1, self.sword)
+        magic_dice = random.randint(1, self.magic)
+        bow_dice = random.randint(1, self.bow)
         if sword_dice >= magic_dice and sword_dice >= bow_dice:
             print("attack with sword")
-            return sword_dice
+            arm = "sword"
+            return arm, sword_dice
         elif bow_dice >= sword_dice and bow_dice >= magic_dice:
             print("attack with bow")
-            return bow_dice
+            arm = "bow"
+            return arm, bow_dice
         elif magic_dice >= sword_dice and magic_dice >= bow_dice:
             print("attack with magic")
-            return magic_dice
+            arm = "magic"
+            return arm, magic_dice
 
-    def defend(test):
+    def defend(self, arm, dice_value):
         hp = 12
-        atk_dice = Warrior.atk(test)
+        if arm == "sword":
+            defend_dice = random.randint(1, self.sword)
+        elif arm == "magic":
+            defend_dice = random.randint(1, self.magic)
+        else:
+            defend_dice = random.randint(1, self.bow)
         print("Hp = " + str(hp))
-        defend_dice = random.randint(1, 10)
-        print("attack " + str(atk_dice))
+        print("attack " + str(dice_value))
         print("defend " + str(defend_dice))
-        if atk_dice > defend_dice:
-            hp -= atk_dice
-            print("Hp -" + str(atk_dice))
+        if dice_value > defend_dice:
+            hp -= dice_value
+            print("Hp -" + str(dice_value))
         print("Current hp = " + str(hp))
         print("##################")
 
 
 class Wizard(Character):
-    name = "Gandalf"
-    Sword = 8
-    Magic = 12
-    Bow = 10
+    chara_class = "wizard"
+    sword = 8
+    magic = 12
+    bow = 10
 
-    def atk(test):
-        sword_dice = random.randint(1, Wizard.Sword)
-        magic_dice = random.randint(1, Wizard.Magic)
-        bow_dice = random.randint(1, Wizard.Bow)
-        print(str(Wizard.name) + " attack!")
+    def attack(self):
+        sword_dice = random.randint(1, self.sword)
+        magic_dice = random.randint(1, self.magic)
+        bow_dice = random.randint(1, self.bow)
         if magic_dice >= sword_dice and magic_dice >= bow_dice:
             print("attack with magic")
-            return magic_dice
+            arm = "magic"
+            return arm, magic_dice
         elif bow_dice >= sword_dice and bow_dice >= magic_dice:
             print("attack with bow")
-            return bow_dice
+            arm = "bow"
+            return arm, bow_dice
         elif sword_dice >= magic_dice and sword_dice >= bow_dice:
             print("attack with sword")
-            return sword_dice
+            arm = "sword"
+            return arm, sword_dice
 
-    def defend(test):
+    def defend(self, arm, dice_value):
         hp = 12
-        atk_dice = Wizard.atk(test)
+        if arm == "sword":
+            defend_dice = random.randint(1, self.sword)
+        elif arm == "magic":
+            defend_dice = random.randint(1, self.magic)
+        else:
+            defend_dice = random.randint(1, self.bow)
         print("Hp = " + str(hp))
-        defend_dice = random.randint(1, 10)
-        print("attack " + str(atk_dice))
+        print("attack " + str(dice_value))
         print("defend " + str(defend_dice))
-        if atk_dice > defend_dice:
-            hp -= atk_dice
-            print("Hp -" + str(atk_dice))
+        if dice_value > defend_dice:
+            hp -= dice_value
+            print("Hp -" + str(dice_value))
         print("Current hp = " + str(hp))
         print("##################")
 
 
 class Archer(Character):
-    name = "Legolas"
-    Sword = 10
-    Magic = 8
-    Bow = 12
+    chara_class = "archer"
+    sword = 10
+    magic = 8
+    bow = 12
 
-    def atk(test):
-        sword_dice = random.randint(1, Archer.Sword)
-        magic_dice = random.randint(1, Archer.Magic)
-        bow_dice = random.randint(1, Archer.Bow)
-        print(str(Archer.name) + " attack!")
+    def attack(self):
+        sword_dice = random.randint(1, self.sword)
+        magic_dice = random.randint(1, self.magic)
+        bow_dice = random.randint(1, self.bow)
         if bow_dice >= sword_dice and bow_dice >= magic_dice:
             print("attack with bow")
-            return bow_dice
+            arm = "bow"
+            return arm, bow_dice
         elif sword_dice >= magic_dice and sword_dice >= bow_dice:
             print("attack with sword")
-            return sword_dice
+            arm = "sword"
+            return arm, sword_dice
         elif magic_dice >= sword_dice and magic_dice >= bow_dice:
             print("attack with magic")
-            return magic_dice
+            arm = "magic"
+            return arm, magic_dice
 
-    def defend(test):
+    def defend(self, arm, dice_value):
         hp = 12
-        atk_dice = Archer.atk(test)
+        if arm == "sword":
+            defend_dice = random.randint(1, self.sword)
+        elif arm == "magic":
+            defend_dice = random.randint(1, self.magic)
+        else:
+            defend_dice = random.randint(1, self.bow)
         print("Hp = " + str(hp))
-        defend_dice = random.randint(1, 10)
-        print("attack " + str(atk_dice))
+        print("attack " + str(dice_value))
         print("defend " + str(defend_dice))
-        if atk_dice > defend_dice:
-            hp -= atk_dice
-            print("Hp -" + str(atk_dice))
+        if dice_value > defend_dice:
+            hp -= dice_value
+            print("Hp -" + str(dice_value))
         print("Current hp = " + str(hp))
         print("##################")
 
 
-# def init():
-#     nb_of_player = input("How many players ? ")
-#     player_class_table = {}
-#     for player in range(int(nb_of_player)):
-#         print(f"Player {player+1}:")
-#         name_choice = input("Chose your name: ")
-#         class_choice = input("chose your class between Warrior, Wizard and Archer: ").lower()
-#         if class_choice == "warrior":
-#             player_class_table[name_choice] = Warrior()
-#         elif class_choice == "wizard":
-#             player_class_table[name_choice] = Wizard()
-#         elif class_choice == "archer":
-#             player_class_table[name_choice] = Archer()
-#     return player_class_table
+gimli = Warrior("Gimli")
+legolas = Archer("Legolas")
+gandalf = Wizard("Gandalf")
 
+print(gimli.name + " the " + gimli.chara_class)
+arm, dice_value = gimli.attack()
+legolas.defend(arm, dice_value)
 
-# player_class_table = init()
-# print(player_class_table)
-test = Warrior()
-Warrior.defend(test)
-test = Wizard()
-Wizard.defend(test)
-test = Archer()
-Archer.defend(test)
+print(gandalf.name + " the " + gandalf.chara_class)
+arm, dice_value = gandalf.attack()
+gimli.defend(arm, dice_value)
 
-
+print(legolas.name + " the " + legolas.chara_class)
+arm, dice_value = legolas.attack()
+gandalf.defend(arm, dice_value)
