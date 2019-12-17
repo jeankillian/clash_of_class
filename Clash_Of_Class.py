1c0602a7f025f5cb70daf3147465b6ea9b2dfef0
LaetitiaDaCosta <laetitiadacosta@mailoo.org>
2019-12-17 09:43:11 +0100

    killian tu es mauvais

--- Clash_Of_Class.py
+++ Clash_Of_Class.py
@@ -140,39 +140,3 @@ class Archer(Character):
         return arm, dice_value
 
 
-gimli = Warrior("Gimli")
-legolas = Archer("Legolas")
-gandalf = Wizard("Gandalf")
-
-# Attack 1: gimli attack legolas
-print(gimli)
-if gimli.current_life_point <= 0:
-    print("|*DEAD*")
-else:
-    arm, dice_value = gimli.attack()
-    legolas.defend(arm, dice_value)
-# Attack 2: legolas attack gandalf
-print(legolas)
-if legolas.current_life_point <= 0:
-    print("|*DEAD*")
-else:
-    arm, dice_value = legolas.attack()
-    gandalf.defend(arm, dice_value)
-# Attack 3: gandalf attack gimli
-print(gandalf)
-if gandalf.current_life_point <= 0:
-    print("|*DEAD*")
-else:
-    arm, dice_value = gandalf.attack()
-    gimli.defend(arm, dice_value)
-
-print(gimli)
-a = gimli.height
-b = gimli.weight
-print(legolas)
-c = legolas.height
-d = legolas.weight
-print(gandalf)
-e = gandalf.height
-f = gandalf.weight
-
